5e281b0a8abdf068bead404e156aa42ed735c0ac
Eduardo Ruiz <eduardo.j.ruiz@gmail.com>
2022-05-12 07:03:34 -0400

    Fixing knot definition. Adding UK and US units directories to init.

--- unum/units/__init__.py
+++ unum/units/__init__.py
@@ -2,6 +2,8 @@
 
 from unum.units.others import *
 from unum.units.custom import *
+from unum.units.imp_UK import *
+from unum.units.US_Customary import *
 
 from unum import Unum
 unitless = Unum(1)
